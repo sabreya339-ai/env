aab12bc85f590796927223658b30b0adb1ab5469
sabreya339-ai <sabreya339@gmail.com>
2026-07-04 16:09:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function checkInputs() {
+
+let f1 = document.getElementById("field1").value;
+let f2 = document.getElementById("field2").value;
+let f3 = document.getElementById("field3");
+let f4 = document.getElementById("field4");
+let msg =document.getElementById("message");
+
+if (f1 === "" || f2 === "" || f3 === "" || f4 === "") {
+  msg.innerText = "Please Enter Your Information";
+  msg.style.color =  "red";
+} else {
+  msg.innerText = "Information Had Sent";
+  msg.style.color = "#6B8E23";
+  }
+}
+
+function showText() {
+  let text = document.getElementById("myText");
+  text.style.display = "block";
+}
+
+function showText2() {
+  let text = document.getElementById("myText2");
+  text.style.display = "block";
+}
